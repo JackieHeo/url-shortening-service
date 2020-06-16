38093ce6e801b2acdd1641667c569dfb5c400e5a
JackieHeo <nkylion@naver.com>
2020-06-17 06:47:45 +0900

    init

--- src/main/java/com/musinsa/urlshortening/controller/UrlShortenBatchController.java
+++ src/main/java/com/musinsa/urlshortening/controller/UrlShortenBatchController.java
@@ -2,7 +2,6 @@ package com.musinsa.urlshortening.controller;
 
 import com.musinsa.urlshortening.service.UrlShortenService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +13,7 @@ public class UrlShortenBatchController {
     private final UrlShortenService urlShortenService;
 
     @ResponseBody
-    //@PostMapping(value = "/batch/url-shorten/sync")
-    @GetMapping(value = "/batch/url-shorten/sync")
+    @PostMapping(value = "/batch/url-shorten/sync")
     public void syncCount() {
         urlShortenService.syncCount();
     }
